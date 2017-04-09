ee5f7813a11733f3ef9bca17b8fa213793cd05f0
Pedro Henrique Farias Cassiano <pf.cassiano@gmail.com>
2017-04-09 15:44:58 -0300

    Interfaces remotas

---
+++ scr/remote/Part.java
@@ -0,0 +1,35 @@
+package remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface Part extends Remote {
+	
+	long getCode() throws RemoteException;
+	
+	String getName() throws RemoteException;
+	
+	String getDescrition() throws RemoteException;
+	
+	/**
+	 * @return Nome do ropositorio original da peca
+	 * @throws RemoteException
+	 */
+	String getRepository() throws RemoteException;
+	
+	/**
+	 * @return Se eh uma peca primitiva ou agregacao de pecas
+	 * @throws RemoteException
+	 */
+	String type() throws RemoteException;
+	
+	int getNumDirectSubparts() throws RemoteException;
+	
+	int getNumPrimitiveSubparts() throws RemoteException;
+	
+	List<Part> getSubparts() throws RemoteException;
+	
+	String getInfo() throws RemoteException;
+
+}
---
+++ scr/remote/PartRepository.java
@@ -0,0 +1,26 @@
+package remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface PartRepository extends Remote {
+
+	String getName() throws RemoteException;
+	
+	long getNumParts() throws RemoteException;
+	
+	List<Part> getParts() throws RemoteException;
+	
+	Part getPart(long cod) throws RemoteException;
+	
+	/**
+	 * Cria uma nova peca no repositorio
+	 * 
+	 * @param nome
+	 * @param descricao
+	 * @param subparts - Lista de pecas que compoem a nova peca
+	 * @throws RemoteException
+	 */
+	void addPart(String nome, String descricao, List<Part> subparts) throws RemoteException;
+}
